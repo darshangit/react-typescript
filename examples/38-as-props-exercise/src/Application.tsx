0de46381825c08342b808aabe667bb56c05e5321
Darshan Satya <darshans1989@gmail.com>
2022-04-14 08:39:10 -0400

    typescript advanced

--- examples/38-as-props-exercise/src/Application.tsx
+++ examples/38-as-props-exercise/src/Application.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
 
-type TextProps = {
-  children: string;
+type TextProps<E> = {
+  children?: string;
+  as?: E;
 };
 
-const exampleText =
-  'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
+type CombinedProps<E extends React.ElementType> = TextProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextProps<E>>;
 
-function Text({ children }: TextProps) {
-  return <div>{children}</div>;
+const exampleText = 'Demo TExt.';
+
+const defaultEle = 'div';
+function Text<E extends React.ElementType = typeof defaultEle>({
+  children,
+  as
+}: CombinedProps<E>) {
+  const TagName = as || defaultEle;
+  return <TagName>{children}</TagName>;
 }
 
 const Application = () => {
   return (
     <main>
-      <Text>{exampleText}</Text>
+      <Text as="p">{exampleText}</Text>
     </main>
   );
 };
